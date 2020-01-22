32b19f2f674e3c60b2ddae29be9faf3309e335d3
calm-block-7153 <rebeccahunt034@gmail.com>
2020-01-22 19:39:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,40 @@
-
+require 'pry'
+class CashRegister
+  attr_accessor :total, :discount, :items, :price, :last_transaction
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  def total
+    @total
+  end
+  
+  def add_item(title, price, quantity = 1)
+   self.total += (price*quantity)  #self = instance of the class += still invokes the method, setter method
+   @items << ((title)*(quantity))
+  end
+  
+  def apply_discount
+   if discount == 0
+    return "There is no discount to apply."
+   else self.total -= self.total*discount/100
+    return "After the discount, the total comes to $#{total}."
+   end
+  end
+  
+  def items
+    @items
+  end
+  
+  def void_last_transaction
+     self.total -= @last_transation.to_f
+  end
+  
+  
+  
+  
+end
+  
